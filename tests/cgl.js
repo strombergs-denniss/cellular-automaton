c37dad6a4dcdf51745690c5b997a4b725c83ea42
strombergs-denniss <59571919+strombergs-denniss@users.noreply.github.com>
2020-02-19 15:46:44 +0200

    Added Conway's Game of Life to tests

--- tests/cgl.js
+++ tests/cgl.js
@@ -1,33 +1,57 @@
-    /*
-        for (let a = 0; a < data.length / 3; a++) {
-            let b = textureWidth * textureHeight;
+import CA from "../core/ca.js"
+import Utility from "../core/utility.js"
+
+class CGL extends CA {
+    constructor(width, height) {
+        super(width, height)
+    }
+
+    randomize() {
+        for (let a = 0; a < this.width * this.height * 3; a += 3) {
+            let val = Math.random() > 0.5 ? 255 : 0;
+            this.colors[a] = val;
+            this.colors[a + 1] = val;
+            this.colors[a + 2] = val;
+        }
+    }
+
+    setup() {
+        this.randomize();
+    }
+
+    update() {
+        for (let a = 0; a < this.width * this.height; a++) {
+            let b = this.width * this.height;
 
             let neighbourCount =
-                (data[(a - 1) % b * 3] == 255) +
-                (data[(a + 1) % b * 3] == 255) +
-                (data[(a + textureWidth) % b * 3] == 255) +
-                (data[(a - textureWidth) % b * 3] == 255) +
-                (data[(a + textureWidth - 1) % b * 3] == 255) +
-                (data[(a + textureWidth + 1) % b * 3] == 255) +
-                (data[(a - textureWidth - 1) % b * 3] == 255) +
-                (data[(a - textureWidth + 1) % b * 3] == 255);
+                (this.colors[(a - 1) % b * 3] == 255) +
+                (this.colors[(a + 1) % b * 3] == 255) +
+                (this.colors[(a + this.width) % b * 3] == 255) +
+                (this.colors[(a - this.width) % b * 3] == 255) +
+                (this.colors[(a + this.width - 1) % b * 3] == 255) +
+                (this.colors[(a + this.width + 1) % b * 3] == 255) +
+                (this.colors[(a - this.width - 1) % b * 3] == 255) +
+                (this.colors[(a - this.width + 1) % b * 3] == 255);
 
 
-            if (data[a * 3] == 0) {
+            if (this.colors[a * 3] == 0) {
                 if (neighbourCount == 3) {
-                    data[a * 3 + 1] = 255;
+                    this.colors[a * 3 + 1] = 255;
                 }
             }
 
-            if (data[a * 3] == 255) {
+            if (this.colors[a * 3] == 255) {
                 if (neighbourCount < 2 || neighbourCount > 3) {
-                    data[a * 3 + 1] = 0;
+                    this.colors[a * 3 + 1] = 0;
                 }
             }
         }
 
-        for (let a = 0; a < textureWidth * textureHeight; a++) {
-            data[a * 3] = data[a * 3 + 1];
-            data[a * 3 + 2] = data[a * 3 + 1];
+        for (let a = 0; a < this.width * this.height; a++) {
+            this.colors[a * 3] = this.colors[a * 3 + 1];
+            this.colors[a * 3 + 2] = this.colors[a * 3 + 1];
         }
-    */
+    }
+}
+
+export default CGL
--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,9 @@
 import LA from "./la.js"
+import CGL from "./cgl.js"
 
 class Test {
     constructor() {
+        //this.ca = new CGL(512, 512);
         this.ca = new LA(512, 512);
     }
 }
